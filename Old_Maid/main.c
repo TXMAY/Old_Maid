83f6887f2703e9357ca717d16ea17a0d0ef5389f
TXMAY <juya6388king@naver.com>
2024-04-30 21:49:30 +0900

    Add Font Setting

--- Old_Maid/main.c
+++ Old_Maid/main.c
@@ -6,9 +6,20 @@
 
 int main(void)
 {
-	// 전체화면 설정
+	// 전체화면 설정(함수가 32비트(x86)전용이라 디버깅 모드를 x86으로 설정해야 함)
 	SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
 	system("mode con:cols=240 lines=67");
+
+	// 폰트 및 폰트 크기 설정
+	COORD fontSize = { 8, 16 };	// 폰트 폭 및 너비(굴림체 16pt 기준)
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;	// 0번 폰트를 기본으로 사용
+	cfi.dwFontSize = fontSize;
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	wcscpy_s(cfi.FaceName, LF_FACESIZE, L"굴림체");
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
 
 	// 마우스 입력 방지
 	HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
